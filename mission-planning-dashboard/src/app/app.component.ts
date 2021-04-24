e44ea23a921b6353e7c8807fac42ce0886af9e3d
JediTiger <JediTiger@users.noreply.github.com>
2021-04-24 17:21:46 -0500

    Added components

--- mission-planning-dashboard/src/app/app.component.ts
+++ mission-planning-dashboard/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mission-planning-dashboard';
+  title = 'Mission Planning Dashboard';
 }
